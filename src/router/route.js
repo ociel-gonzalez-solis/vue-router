49d1d5ac76f1a3215ba6c15eb5241c218f1b722b
ocielgonzalezsolis <ocielgonzalezsolis@hotmail.com>
2023-07-24 17:33:11 -0700

    router ready.

--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,7 @@
 import AboutPage from "@/moduls/pokemon/pages/AboutPage.vue";
 import ListPage from "@/moduls/pokemon/pages/ListPage.vue";
 import PokemonPage from "@/moduls/pokemon/pages/PokemonPage.vue";
+import NoPageFound from "@/moduls/pokemon/pages/NoPageFound.vue";
 import { createWebHashHistory } from "vue-router";
 import { createRouter } from "vue-router";
 
@@ -8,7 +9,7 @@ const routes = [
   { path: "/", component: ListPage },
   { path: "/about", component: AboutPage },
   { path: "/id", component: PokemonPage },
-  { path: "/:pathMatch(.*)*", component: AboutPage },
+  { path: "/:pathMatch(.*)*", component: NoPageFound },
 ];
 
 const router = createRouter({
