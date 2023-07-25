3bdcd79d118164ba7f2d7fc0cd99eaadad8fc7b2
ocielgonzalezsolis <ocielgonzalezsolis@hotmail.com>
2023-07-25 16:24:31 -0700

    Lazy Login

--- src/router/route.js
+++ src/router/route.js
@@ -1,4 +1,3 @@
-import AboutPage from "@/moduls/pokemon/pages/AboutPage.vue";
 import ListPage from "@/moduls/pokemon/pages/ListPage.vue";
 import PokemonPage from "@/moduls/pokemon/pages/PokemonPage.vue";
 import NoPageFound from "@/moduls/pokemon/pages/NoPageFound.vue";
@@ -7,7 +6,13 @@ import { createRouter } from "vue-router";
 
 const routes = [
   { path: "/", component: ListPage },
-  { path: "/about", component: AboutPage },
+  {
+    path: "/about",
+    component: () =>
+      import(
+        /* webpackChunkName: 'AboutPage' */ "@/moduls/pokemon/pages/AboutPage"
+      ),
+  },
   { path: "/id", component: PokemonPage },
   { path: "/:pathMatch(.*)*", component: NoPageFound },
 ];
@@ -16,6 +21,5 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
-
 
 export default router;
